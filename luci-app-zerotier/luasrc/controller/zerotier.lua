f81f81094d335b5ad8c3ee54970179fde1776f33
jjm2473 <1129525450@qq.com>
2023-10-06 13:38:53 +0800

    luci-app-zerotier: rename init script

--- luci-app-zerotier/luasrc/controller/zerotier.lua
+++ luci-app-zerotier/luasrc/controller/zerotier.lua
@@ -5,9 +5,9 @@ function index()
 		return
 	end
 
-	entry({"admin","vpn"}, firstchild(), "VPN", 45).dependent = false
+	entry({"admin", "vpn"}, firstchild(), "VPN", 45).dependent = false
 
-	entry({"admin", "vpn", "zerotier"},firstchild(), _("ZeroTier")).dependent = false
+	entry({"admin", "vpn", "zerotier"}, firstchild(), _("ZeroTier")).dependent = false
 
 	entry({"admin", "vpn", "zerotier", "general"}, cbi("zerotier/settings"), _("Base Setting"), 1)
 	entry({"admin", "vpn", "zerotier", "log"}, form("zerotier/info"), _("Interface Info"), 2)
--- luci-app-zerotier/luasrc/model/cbi/zerotier/settings.lua
+++ luci-app-zerotier/luasrc/model/cbi/zerotier/settings.lua
@@ -1,8 +1,9 @@
-a = Map("zerotier")
-a.title = translate("ZeroTier")
-a.description = translate("Zerotier is an open source, cross-platform and easy to use virtual LAN")
 
-a:section(SimpleSection).template = "zerotier/zerotier_status"
+local a, t, e
+
+a = Map("zerotier", translate("ZeroTier"),
+	translate("Zerotier is an open source, cross-platform and easy to use virtual LAN"))
+a:section(SimpleSection).template  = "zerotier/zerotier_status"
 
 t = a:section(NamedSection, "sample_config", "zerotier")
 t.anonymous = true
@@ -16,12 +17,12 @@ e = t:option(DynamicList, "join", translate('ZeroTier Network ID'))
 e.password = true
 e.rmempty = false
 
-e = t:option(Flag, "nat", translate("Auto NAT Clients"))
-e.description = translate("Allow zerotier clients access your LAN network")
+e = t:option(Flag, "nat", translate("Auto NAT Clients"), translate("Allow zerotier clients access your LAN network"))
 e.default = 0
 e.rmempty = false
 
-e = t:option(DummyValue, "opennewwindow", translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Zerotier.com\" onclick=\"window.open('https://my.zerotier.com/network')\" />"))
-e.description = translate("Create or manage your zerotier network, and auth clients who could access")
+e = t:option(DummyValue, "opennewwindow" , 
+	translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Zerotier.com\" onclick=\"window.open('https://my.zerotier.com/network')\" />"),
+	translate("Create or manage your zerotier network, and auth clients who could access"))
 
 return a
